a5f23407a9db65902b26dc0984a3f3920a66292a
Nagy Samu <nagy.samu222@gmail.com>
2015-08-26 19:48:35 +0200

    new: Add default build files new: Add some basic Url class test

---
+++ .test/HttpUrlTest.php
@@ -0,0 +1,77 @@
+<?php
+
+class HttpUrlTest extends PHPUnit_Framework_TestCase {
+
+  public function testInit() {
+    \Framework::setup( function () {
+    } );
+  }
+
+  /**
+   * @depends      testInit
+   *
+   * @dataProvider provider
+   */
+  public function testParse( $url, $components ) {
+
+    $this->assertEquals( $components, \Http\Url::instance( $url )->component );
+    $this->assertEquals( $url, \Http\Url::instance( $url )->build() );
+  }
+
+  public function provider() {
+    return [
+      [ 'https://test-cloud-pln.pbcs.us1.oraclecloud.com/workspace', [
+        'query'  => [ ],
+        'scheme' => 'https',
+        'host'   => 'test-cloud-pln.pbcs.us1.oraclecloud.com',
+        'path'   => '/workspace'
+      ]
+      ],
+      [
+        'http://www.example.com/path?section=17', [
+        'query'  => [
+          'section' => '17'
+        ],
+        'scheme' => 'http',
+        'host'   => 'www.example.com',
+        'path'   => '/path'
+      ]
+      ], [
+        'http://www.example.com:8080/path?section=17', [
+          'query'  => [
+            'section' => '17'
+          ],
+          'port'   => 8080,
+          'scheme' => 'http',
+          'host'   => 'www.example.com',
+          'path'   => '/path'
+        ]
+      ], [ 'ftp://username:password@example.com:9242', [
+        'query'    => [ ],
+        'scheme'   => 'ftp',
+        'port'     => 9242,
+        'user'     => 'username',
+        'password' => 'password',
+        'host'     => 'example.com'
+      ]
+      ], [ '//username:password@example.com:9242', [
+        'query'    => [ ],
+        'port'     => 9242,
+        'user'     => 'username',
+        'password' => 'password',
+        'host'     => 'example.com'
+      ]
+      ], [ '//username:password@example.com:9242/?foo=400', [
+        'query'    => [
+          'foo' => 400
+        ],
+        'port'     => 9242,
+        'user'     => 'username',
+        'password' => 'password',
+        'host'     => 'example.com',
+        'path'     => '/'
+      ]
+      ]
+    ];
+  }
+}
